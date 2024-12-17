08560374bb07fac51c063b8ab9480b72e85dc0ac
atiasultani <160133182+atiasultani@users.noreply.github.com>
2024-12-18 04:28:17 +0500

    grid2

--- grid2/src/app/page.tsx
+++ grid2/src/app/page.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='grid grid-cols-2 grid-rows-3 px-7 py-8'>
+    <div className='flex'>
+    <div className='w-screen grid grid-cols-3 grid-rows-3 px-7 py-8'>
       
-      <div 
-      className='div1 bg-yellow-400 w-[120px] h-[500px] gap-x-8 gap-y-8 outline outline-white'>Nav</div>
-      <div className= 'div2 -ml-[32em] bg-yellow-400 w-[820px] h-20 gap-x-8 gap-y-8 outline outline-white'> Header</div>
-      <div className='div3 ml-32 -mt-[25em]  bg-yellow-400 w-[700px] h-[350px] gap-x-8 gap-y-8 outline outline-white'>cont1</div>
-      <div className='div4 ml-48 -mt-[25em] bg-yellow-400 w-[120px] h-[400px] gap-x-8 gap-y-8 outline outline-white'>Ads</div>
-      <div className='div5 ml-32 -mt-[33em] bg-yellow-400 w-[695px] h-12 gap-x-8 gap-y-8 outline outline-white'>Footer</div>
+      <div className='div1 ml-7  mt-[5.5em] bg-cyan-600 w-[100px] h-[320px] gap-x-8 gap-y-8 outline outline-4 outline-black'>Nav</div>
+      <div className= 'div2 -ml-[25em] bg-purple-800 w-[500px] h-20  gap-y-8 outline outline-4 outline-black'> Header</div>
+      <div className='div3  -ml-[45em] mt-[5.5em]  bg-blue-400 w-[392px] h-[220px]  gap-x-8 gap-y-8 outline outline-4 outline-black'>cont1</div>
+      <div className='div3 ml-[133px] -mt-[8em]  bg-blue-800 w-[200px] h-[120px] gap-x-8 gap-y-8 outline outline-4 outline-black'>cont1</div>
+      <div className='div4 -ml-[6em] -mt-[8em] bg-blue-400 w-[195px] h-[120px] gap-x-0 gap-y-8 outline outline-4 outline-black'>Ads</div>
+      <div className='div5 -ml-[51.6em]  bg-purple-800 w-[500px] h-[80px] gap-x-8 gap-y-8 outline outline-4 outline-black '>Footer</div>
 
 
+    </div>
     </div>
   )
 }
